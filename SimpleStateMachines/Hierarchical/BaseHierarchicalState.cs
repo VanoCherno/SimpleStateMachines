36bcdb08dfd0463209a3484b4b2a64eaa3a5b09b
VanoCherno <thevanocherno@gmail.com>
2025-10-11 16:08:59 +0700

    Update BaseHierarchicalState.cs

--- SimpleStateMachines/Hierarchical/BaseHierarchicalState.cs
+++ SimpleStateMachines/Hierarchical/BaseHierarchicalState.cs
@@ -3,8 +3,9 @@
     /// <summary>
     /// An empty base class that enforces inheritance constraints
     /// </summary>
-    public abstract class BaseHierarchicalState<TId, TState> : BaseState<TId> where TState : BaseHierarchicalState<TId, TState>
+    public abstract class BaseHierarchicalState<TId> : BaseState<TId>
     {
         protected BaseHierarchicalState(TId id) : base(id) {}
     }
 }
+
